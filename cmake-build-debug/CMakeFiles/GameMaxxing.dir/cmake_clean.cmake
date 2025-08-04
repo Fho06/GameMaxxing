e615bd2bfa92b3c7371b98c4d7b7ff341e38b363
Diego G S Biondi <diegogsbiondi@gmail.com>
2025-08-03 23:02:18 -0400

    SFML Is Broken
    What it says in the title

---
+++ cmake-build-debug/CMakeFiles/GameMaxxing.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameMaxxing.dir/src/DatasetHLTB.cpp.obj"
+  "CMakeFiles/GameMaxxing.dir/src/DatasetHLTB.cpp.obj.d"
+  "CMakeFiles/GameMaxxing.dir/src/main.cpp.obj"
+  "CMakeFiles/GameMaxxing.dir/src/main.cpp.obj.d"
+  "GameMaxxing.exe"
+  "GameMaxxing.exe.manifest"
+  "GameMaxxing.pdb"
+  "libGameMaxxing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameMaxxing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::Window window(
+        sf::VideoMode(sf::Vector2u(640, 480)),
+        "Hello World");
+    return 0;
+}
